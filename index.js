714a06d99c0de41b0e2c3b1153f6df2496646bd3
victormuteithia <muteithiavictor@gmail.com>
2023-10-12 13:43:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1*num2;
 
-// Write your code here
+const random = Math.round(Math.random()) +1;
+
+const num3 = 18;
+const num4 = 7;
+const mod = num3%num4;
+
+const max = Math.max(2,4,6,8,10,12,14,16,18,20);
